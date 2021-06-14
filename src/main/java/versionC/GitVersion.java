da550c5a41be904a94c59f293ac6bb94b746b60b
adex4545 <adebolaa015@gmail.com>
2021-06-14 17:15:39 +0100

    Initiate:2nd code added

--- src/main/java/versionC/GitVersion.java
+++ src/main/java/versionC/GitVersion.java
@@ -1,11 +1,11 @@
 package versionC;
 
 
-
 public class GitVersion {
 
     public static void main(String[] args) {
 
-        System.out.println( "Hello Aisha");
+        System.out.println("Hello Aisha");
+        System.out.println("Hello Yasser");
     }
 }
