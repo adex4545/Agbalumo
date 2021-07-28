4132e043092e1e35f69dcebd67fe2d38ab06a5e2
adex4545 <adebolaa015@gmail.com>
2021-07-28 22:33:22 +0100

    Try push to repo

--- src/main/java/versionC/SecondPush.java
+++ src/main/java/versionC/SecondPush.java
@@ -3,6 +3,8 @@ package versionC;
 public class SecondPush {
 
     public static void main(String[] args) {
-        System.out.println( "Cloudy today! Friday!!!");
+        System.out.println( "Cloudy today? Tuesday!!!");
+        System.out.println( "Sunny today ?  Wednesday");
     }
 }
+
