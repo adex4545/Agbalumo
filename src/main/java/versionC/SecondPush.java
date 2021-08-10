28cf6d4e70ace83dae436dd036a8a24c601e15c3
adex4545 <adebolaa015@gmail.com>
2021-08-10 14:03:27 +0100

    new commit,check dis !!!

--- src/main/java/versionC/SecondPush.java
+++ src/main/java/versionC/SecondPush.java
@@ -5,6 +5,7 @@ public class SecondPush {
     public static void main(String[] args) {
         System.out.println( "Cloudy today? Tuesday!!!");
         System.out.println( "Sunny today ?  Wednesday");
+        System.out.println( "JavaScript with Cypress");
     }
 }
 
